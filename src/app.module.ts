d0a2b7ba328ca16e9d6d7e6c772f78138ff608bf
ramarfx <ramagamiing33@gmail.com>
2024-07-18 22:46:09 +0700

    fix ;agiii

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { CommonModule } from './common/common.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [UserModule, CommonModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
