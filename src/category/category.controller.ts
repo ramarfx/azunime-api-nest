6e7ede41d3fc455751ac65cbfc9d65d9edc4129b
ramarfx <ramagamiing33@gmail.com>
2024-07-19 10:09:39 +0700

    store category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from '@nestjs/common';
+import { CategoryService } from './category.service';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+
+  @Post()
+  async storeCategory(@Body() req: { name: string }) {
+    try {
+      const result = await this.categoryService.storeCategory(req.name);
+
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { Category } from '@prisma/client';
+import { PrismaService } from 'src/common/prisma.service';
 
 @Injectable()
-export class CategoryService{}
+export class CategoryService {
+  constructor(private prismaService: PrismaService) {}
+
+  async storeCategory(category: string): Promise<Category> {
+    const categoryReq = await this.prismaService.category.create({
+      data: { name: category },
+    });
+
+    return categoryReq;
+  }
+}
